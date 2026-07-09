381c3b78e814b0f0bcef1c8bb33e278c9de9d492
stutibisht8 <stutibisht8@gmail.com>
2026-07-09 11:28:46 +0530

    Delete Translator.py

--- Translator.py
+++
@@ -1,7 +0,0 @@
-from deep_translator import GoogleTranslator
-
-text = input("Enter text: ")
-
-translated = GoogleTranslator(source="auto", target="hi").translate(text)
-
-print("Translated Text:", translated)
